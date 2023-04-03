be8be950359d6fcb7af0b0ac342ef2692da27657
jaykumar <slapbot6@gmail.com>
2023-04-02 22:33:12 -0400

    removed useless file

--- globalConstants.js
+++
@@ -1,6 +0,0 @@
-export const BLOGPOST_TAGS = {
-    AI : Symbol("artificialIntelligence"),
-    BLOCKCHAIN : Symbol("blockchain"),
-    DEVOPS : Symbol("devops"),
-    WEB : Symbol("webDevelopment")
-}
