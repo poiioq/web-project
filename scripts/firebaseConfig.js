d9d174c6d72efc803361a18c619930d4ebc6845f
jaykumar <slapbot6@gmail.com>
2023-09-14 12:24:02 -0400

    changed config

--- scripts/firebaseConfig.js
+++ scripts/firebaseConfig.js
@@ -1,17 +1,16 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.19.0/firebase-app.js";
-import { getDatabase, set, get, update, remove, ref, child }
-    from "https://www.gstatic.com/firebasejs/9.19.0/firebase-database.js"
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-database.js"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCCyBVOV-uzycyCz5tiZOrS5yvaW8tXVXM",
-    authDomain: "techsquared-d947b.firebaseapp.com",
-    databaseURL: "https://techsquared-d947b-default-rtdb.firebaseio.com",
-    projectId: "techsquared-d947b",
-    storageBucket: "techsquared-d947b.appspot.com",
-    messagingSenderId: "275235034830",
-    appId: "1:275235034830:web:e3819414b96712c507e29f"
+    apiKey: "AIzaSyD-caOqiBSSt07n-02RTXng-nYxPWogIW0",
+    authDomain: "techsquaredio-38ada.firebaseapp.com",
+    databaseURL: "https://techsquaredio-38ada-default-rtdb.firebaseio.com",
+    projectId: "techsquaredio-38ada",
+    storageBucket: "techsquaredio-38ada.appspot.com",
+    messagingSenderId: "735060439908",
+    appId: "1:735060439908:web:58f6e7469da1a5be8b0071"
 };
 
 // Initialize Firebase
